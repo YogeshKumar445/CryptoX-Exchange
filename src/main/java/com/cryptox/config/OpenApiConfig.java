5b830cd083bef7ef56632caf229ff499276ef30b
YogeshKumar445 <ykyogesh445@gmail.com>
2026-07-23 11:18:07 +0530

    Add Swagger OpenAPI documentation with JWT authentication

---
+++ src/main/java/com/cryptox/config/OpenApiConfig.java
@@ -0,0 +1,42 @@
+package com.cryptox.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+
+        final String securitySchemeName = "bearerAuth";
+
+        return new OpenAPI()
+
+                .info(new Info()
+                        .title("CryptoX Exchange API")
+                        .version("1.0")
+                        .description("REST APIs for CryptoX Exchange Backend")
+                        .contact(new Contact()
+                                .name("Yogesh Kumar")
+                                .email("ykyogesh445@gmail.com")
+                        ))
+
+                .addSecurityItem(new SecurityRequirement()
+                        .addList(securitySchemeName))
+
+                .schemaRequirement(
+                        securitySchemeName,
+                        new SecurityScheme()
+                                .name(securitySchemeName)
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")
+                );
+    }
+}
